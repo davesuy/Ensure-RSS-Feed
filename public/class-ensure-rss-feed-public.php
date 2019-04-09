4c4f96758150ac9a05d10839bb77a05cfbdd91eb
Davesuy <davesuy@gmail.com>
2019-04-09 17:41:16 +0800

    change 20 limits

--- public/class-ensure-rss-feed-public.php
+++ public/class-ensure-rss-feed-public.php
@@ -135,7 +135,7 @@ class Ensure_Rss_Feed_Public {
 				if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
 				    // Figure out how many total items there are, but limit it to 5. 
-				    $maxitems = $rss->get_item_quantity( 8 ); 
+				    $maxitems = $rss->get_item_quantity( 20 ); 
 
 				    // Build an array of all the items, starting with element 0 (first element).
 				    $rss_items = $rss->get_items( 0, $maxitems );
